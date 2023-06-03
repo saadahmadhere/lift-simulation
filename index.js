324b93fdc3aafd8b2fe9789a41c0898e09c490b1
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-06-03 18:31:54 +0530

    [feat]: add basic functionality to the go button

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const floors = document.querySelector('.floor');
+const lifts = document.querySelector('.lift');
+const goButton = document.querySelector('#btn-go');
 
+goButton.addEventListener('click', () => {
+	console.log(floors.value);
+	window.location.href = 'simulation.html';
+});
 
+// console.log(floors.value);
