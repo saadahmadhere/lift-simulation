d04d68ebf48485ea1aad54a3e7ec3498b98d932f
saadahmadhere <52596800+saadahmadhere@users.noreply.github.com>
2023-06-06 23:05:15 +0530

    remove dead code

--- index.js
+++ index.js
@@ -4,11 +4,8 @@ const goButton = document.querySelector('#btn-go');
 console.log(floors.value);
 
 goButton.addEventListener('click', () => {
-	// console.log({ floors, lifts });
 	if (floors.value && lifts.value) window.location.href = 'simulation.html';
 	else {
 		alert('please fill in both boxes');
 	}
 });
-
-// console.log(floors.value);
